5cd9f84ee5866906610e9456e3309cc5f9ca7e6e
Jasmeet Singh <129063092+jassmunday@users.noreply.github.com>
2024-02-03 18:18:05 +0530

    Update Description.jsx

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -6,7 +6,7 @@ function Description() {
     <br></br>
     <section className="body">
       <div className="left-section">
-        <img src="images/d1.png" alt='background image' width={500}></img>
+        <img src="https://github.com/jassmunday/landing-page-practice/assets/129063092/aa4d9eb6-69c7-4888-97f1-86e11639f486" alt='background image' width={500}></img>
         
         
       </div>
